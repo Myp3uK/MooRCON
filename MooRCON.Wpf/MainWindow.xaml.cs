68e348c67f4715ab2d39c7303608c918218b9b2c
Myp3uK <eybrfkmysq16@gmail.com>
2026-07-12 21:14:09 +0300

    feat(wpf): кастомный заголовок окна и иконка приложения
    - собственный title bar: иконка app.ico + название + подзаголовок
    - кнопки заголовка (свернуть/развернуть/закрыть) + переключение развернуть/восстановить
    - иконка app.ico в Assets, exe и заголовке

--- MooRCON.Wpf/MainWindow.xaml.cs
+++ MooRCON.Wpf/MainWindow.xaml.cs
@@ -4,13 +4,23 @@ namespace MooRCON.Wpf;
 
 public partial class MainWindow : Window
 {
-    public MainWindow() => InitializeComponent();
+    private const string MaximizeGlyph = ""; // Segoe MDL2 «развернуть»
+    private const string RestoreGlyph = "";  // Segoe MDL2 «восстановить»
 
-    private void Minimize_Click(object sender, RoutedEventArgs e)
+    public MainWindow()
+    {
+        InitializeComponent();
+        StateChanged += (_, _) => UpdateMaximizeGlyph();
+    }
+
+    private void OnMinimize(object sender, RoutedEventArgs e)
         => WindowState = WindowState.Minimized;
 
-    private void Maximize_Click(object sender, RoutedEventArgs e)
+    private void OnMaximizeRestore(object sender, RoutedEventArgs e)
         => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
 
-    private void Close_Click(object sender, RoutedEventArgs e) => Close();
+    private void OnClose(object sender, RoutedEventArgs e) => Close();
+
+    private void UpdateMaximizeGlyph()
+        => MaximizeButton.Content = WindowState == WindowState.Maximized ? RestoreGlyph : MaximizeGlyph;
 }
